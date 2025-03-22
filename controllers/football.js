6b7c0427891ecc836d45883028838b39bc89b39f
Benjamin Braner <benjamin.branner@gmail.com>
2025-03-21 23:53:34 -0400

    cache api set

--- controllers/football.js
+++ controllers/football.js
@@ -1,11 +1,19 @@
 const { response } = require('express')
+const NodeCache = require('node-cache')
 
 // X-Auth-Token = 43e98340908a407cbaee5d154728e512
 
-const getMatchesFromCompetition = async (req, res = response, next) => {
-	
-	const { leagueId } = req.params;
-	
+const  cache = new NodeCache({ stdTTL: 86400 }) // 24 horas 
+
+const getMatchesFromCompetition = async (req, res = response) => {
+	const { leagueId } = req.params
+
+	// Verifica si ya hay datos en la caché
+	const cachedData = cache.get(leagueId)
+	if (cachedData) {
+		return res.status(200).json({ ok: true, data: cachedData })
+	}
+
 	try {
 		const apiResponse = await fetch(`https://api.football-data.org/v4/competitions/${leagueId}/matches`, {
 			headers: {
@@ -13,23 +21,19 @@ const getMatchesFromCompetition = async (req, res = response, next) => {
 				'Content-Type': 'application/json'
 			}
 		})
-
+		
 		const data = await apiResponse.json()
-
-		return res.status(200).json({
-			ok: true,
-			data
-		})
+		cache.set(leagueId, data) // Guarda en caché por 24 horas
+		console.log("llamada a la api")
+		return res.status(200).json({ ok: true, data })
 	} catch (error) {
 		console.error('Error al obtener los partidos:', error)
-		return res.status(500).json({
-			ok: false,
-			message: 'Error al obtener los datos',
-			error: error.message
-		})
+		return res.status(500).json({ ok: false, message: 'Error al obtener los datos' })
 	}
 }
 
+
+
 module.exports = {
-	getMatchesFromCompetition
+	getMatchesFromCompetition,
 }
--- routes/football.js
+++ routes/football.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const {getMatchesFromCompetition } = require('../controllers/football')
 const router = Router()
-
+const axios = require("axios");
 
 router.get(
 	"/leagues/:leagueId",
@@ -9,4 +9,44 @@ router.get(
 )
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ruta para obtener los precios de USDT en P2P
+router.post("/binance/p2p", async (req, res) => {
+	try {
+	    const url = "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search";
+	    const payload = {
+		   asset: "USDT",  // Criptomoneda a buscar
+		   fiat: "BOB",  // Moneda fiduciaria (cambia según el país)
+		   tradeType: "BUY",  // "BUY" para comprar, "SELL" para vender
+		   page: 1,
+		   rows: 10,
+		   payTypes: [],
+		   publisherType: null
+	    };
+ 
+	    const response = await axios.post(url, payload, {
+		   headers: { "Content-Type": "application/json" }
+	    });
+ 
+	    res.json(response.data);
+	} catch (error) {
+	    console.error("Error al obtener datos de Binance:", error);
+	    res.status(500).json({ error: "Error en la solicitud a Binance" });
+	}
+ });
+ 
 module.exports = router
